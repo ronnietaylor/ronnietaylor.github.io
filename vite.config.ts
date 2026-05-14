0d8509cef342c2ab938807b101f2bf78dcc3a612
Ronnie Taylor <hello@ronnietaylor.com>
2026-05-14 14:41:49 -0700

    Clean up vite config Remove jekyll config Update tagline

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: './src/App.tsx',
-      output: {
-        entryFileNames: 'assets/js/bundle.js',
-        chunkFileNames: 'assets/chunks/[name]-[hash].js',
-        assetFileNames: 'assets/[name][extname]',
-      },
-    },
-  },
 })
